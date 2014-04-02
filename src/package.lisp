8c32784e79df5a4eed74f262db2c1a5136631fdf
Ryan Pavlik <rpavlik@gmail.com>
2014-04-02 15:22:07 -0500

    Actually export the spritesheet-making functions!

--- src/package.lisp
+++ src/package.lisp
@@ -3,4 +3,7 @@
   (:export
 
    ;; font
-   #:make-font-atlas #:make-font-atlas-files))
+   #:make-font-atlas #:make-font-atlas-files
+
+   ;; sprites
+   #:make-sprite-atlas #:make-sprite-atlas-files))
